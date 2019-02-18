d8128615445c1da9d3a47f53cb319c4124975367
Ricardo GR <xrigaru@gmail.com>
2019-02-18 21:46:53 +0100

    actualizacion

--- pec2.py
+++ pec2.py
@@ -10,16 +10,18 @@ from stackapi import StackAPI
 SITE = StackAPI('stackoverflow')
 
 SITE.page_size = 100
-SITE.max_pages = 20
+SITE.max_pages = 100
 
 questions = SITE.fetch('questions', min=20, tagged='python', sort='votes')
 questions
 
-
+'''
 for key in questions:
   print( key, ":", questions[key])
-  
+'''
 qlist = questions['items']  
+
+'''
 qlist[0]
 
 
@@ -40,11 +42,11 @@ for i in range(len(qlist)):
 questions.keys()
 
 type(questions['items'])
-
+'''
 
 import pandas as pd
 df = pd.DataFrame(qlist)
-
+'''
 for i in range(len(qlist)):
     
     qlist[0]['question_id']
@@ -55,3 +57,51 @@ for i in range(len(qlist)):
     qlist[0]['owner'].get('reputation')
     qlist[0]['owner'].get('user_type')
     qlist[0]['answer_count']
+    
+'''    
+df.columns.values
+
+'''qlist2 = df['owner'].values.tolist()'''
+
+owner = pd.DataFrame(df['owner'].values.tolist())
+    
+owner = owner.drop(['accept_rate', 'display_name', 'link', 'profile_image'], axis=1)
+owner.head()
+    
+df = df.drop(['accepted_answer_id', 'closed_date', 'closed_reason', 'community_owned_date', 
+       'last_activity_date', 'last_edit_date', 'link', 'locked_date', 'migrated_from', 
+         'owner', 'protected_date', 'score', 'tags'], axis=1)
+    
+dfPython = pd.concat([df, owner], axis=1, join_axes=[df.index])
+
+dfPython.columns.values
+
+sequence = ['user_id', 'question_id', 'title', 'is_answered',
+            'view_count', 'creation_date', 'reputation',
+            'user_type', 'answer_count']
+
+dfPython = dfPython.reindex(columns=sequence)
+
+sequence = {'user_id': 'IDUser', 
+            'question_id': 'IDQuestion', 
+            'title': 'Title', 
+            'is_answered': 'IsAnswered',
+            'view_count': 'ViewCount', 
+            'creation_date': 'CreationDate', 
+            'reputation': 'Reputation',
+            'user_type': 'UserType', 
+            'answer_count': 'AnswerCount'}
+
+dfPython = dfPython.rename(columns = sequence) 
+
+
+dfPython.loc[dfPython['IsAnswered'] == 'True']
+
+
+
+
+
+
+
+
+
